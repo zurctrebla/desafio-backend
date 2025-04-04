6eec188ee86fea5e14d442ed7c3dabd8982a78e4
Albert Cruz <albertcruz@terra.com.br>
2025-04-04 22:07:17 +0100

    fix: force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
